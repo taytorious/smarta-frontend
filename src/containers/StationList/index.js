050201713ca2ad6749ebb1d59d2915c8482385f9
Micah Prescott <micah.prescott@soltech.net>
2019-06-13 22:31:38 -0400

    Add distance to station list

--- src/containers/StationList/index.js
+++ src/containers/StationList/index.js
@@ -5,6 +5,7 @@ import {brand_lighter_grey} from "../../utils/colors";
 import { Link } from "react-router-dom";
 import api from "../../api";
 
+import LocationIcon from '../../assets/location.svg';
 
 const List = styled.div`
   padding: 0 25px;
@@ -13,12 +14,32 @@ const List = styled.div`
 const ListItem = styled.div`
   width: 100%;
   border-bottom: 1px solid ${brand_lighter_grey};
-  font-size: 4vh;
   display: flex;
-  align-items: center;
+  flex-wrap: wrap;
   justify-content: center;
+  align-items: center;
+  position: relative;
   height: calc(12.5vh - 8.75px);
   cursor: pointer;
+`;
+
+const ListItemName = styled.div`
+    font-size: 4vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const DistanceContainer = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    font-size: 2.5vh;
+    right: 0;
+`;
+
+const DistanceIcon = styled.img`
+    height: 35px;
 `;
 
 const StationList = (props) => {
@@ -33,9 +54,18 @@ const StationList = (props) => {
                 {data.stations.map((station) => {
                     const name = station["station-name"];
                     const key = name.replace(/Station/gi, '').trim().replace(' ', "-");
+                    const distance = station.distance.toFixed(2);
                     return (
                         <Link key={key} to={`/${key}`}>
-                            <ListItem>{name.replace(/Station/gi, '')}</ListItem>
+                            <ListItem>
+                                <ListItemName>
+                                    {name.replace(/Station/gi, '')}
+                                </ListItemName>
+                                <DistanceContainer>
+                                <DistanceIcon src={LocationIcon} />
+                                    {distance} mi
+                                </DistanceContainer>
+                            </ListItem>
                         </Link>
                     );
                 })}
