9edc07ad2113153553a8c1e691595b0520299690
Micah Prescott <micah.prescott@soltech.net>
2019-06-26 07:54:20 -0400

    Adjust environment urls and npm start commands

--- src/env.js
+++ src/env.js
@@ -1,8 +1,17 @@
 const ENV = process.env.REACT_APP_ENV;
 
-const BASE_URL = (ENV === 'prod') || (ENV === 'dev')
-    ? 'http://smarta-api.herokuapp.com' 
-    : 'http://localhost:3006';
+const getBaseUrl = () => {
+    switch(ENV) {
+        case 'prod':
+            return 'https://api.ataper.com';
+        case 'staging':
+            return 'https://staging.api.ataper.com';
+        default:
+            return 'http://localhost:3006';
+    }
+}
+
+const BASE_URL = getBaseUrl();
 
 export {
     ENV,
